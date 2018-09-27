c7be8fdbd5f275d4b4f64ad4cbeffb3d63f037e7
Luca Muratore <luca.muratore@iit.it>
2018-09-28 03:03:20 +0800

    mingata: everything is on /opt/xbot/build/install

--- setup.bash
+++ setup.bash
@@ -6,7 +6,22 @@ touch ~/.xbot/active_config.yaml
 
 # export env variables
 export XBOT_CONFIG=~/.xbot/active_config.yaml
-export XBOT_ROOT=/usr/share/xbot
+export XBOT_ROOT=/opt/xbot/build/install/share/xbot
+export XBOT_HOME=/opt/xbot/build/install
+
+export PATH=$PATH:$XBOT_HOME/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$XBOT_HOME/lib
+export CPATH=$CPATH:$XBOT_HOME/include
+export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:$XBOT_HOME
+
+# ROS packages
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$XBOT_HOME/share
 
 # Gazebo plugins
-export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:/usr/lib
+export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$XBOT_HOME/lib
+
+# Robot Models
+if [ -d $XBOT_ROOT/robots/iit-coman-ros-pkg ]; then
+    export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$XBOT_ROOT/robots/iit-coman-ros-pkg
+    export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$XBOT_ROOT/robots/iit-coman-ros-pkg/coman_gazebo/database
+fi
